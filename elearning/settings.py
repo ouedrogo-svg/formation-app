027ff013827bffaca326d5b4bc2d800a9838df98
OUEDRAOG-SVG <osoumailla95@gmail.com>
2026-05-10 18:03:37 +0000

    migration

--- elearning/settings.py
+++ elearning/settings.py
@@ -54,11 +54,11 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-"""ALLOWED_HOSTS = config(
+ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
     default="localhost,127.0.0.1,quaplus.onrender.com",
     cast=Csv(),
-)"""
+)
 
 
 # Application definition
