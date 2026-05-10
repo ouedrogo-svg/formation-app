fa9aabe1191471f577253984dbdd2bef88cad09c
OUEDRAOG-SVG <osoumailla95@gmail.com>
2026-05-10 17:53:45 +0000

    migration

--- elearning/settings.py
+++ elearning/settings.py
@@ -50,11 +50,7 @@ for _env in (BASE_DIR / ".env", BASE_DIR / ".venv" / ".env"):
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config(
-    "SECRET_KEY",
-    default="django-insecure-kmu%^_tlwl^(!b%h4v-(l)6^pr&nkve_!*3qd__o2g06e@bf07",
-)
-
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
